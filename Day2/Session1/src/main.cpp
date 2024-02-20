fa7780b35ef93260e51d98fb8089f75a96aed9bf
Trace N. Robbins <53279437+R-Trace@users.noreply.github.com>
2024-02-20 14:25:39 +0100

    red be anthony

--- Day2/Session1/src/main.cpp
+++ Day2/Session1/src/main.cpp
@@ -6,7 +6,7 @@
 // [x] The Green LED is also turned on.
 // [x] The Blue LED is also turned on.
 // [x] 300 milliseconds after all the LEDs turn on, the Blue LED turns off.
-// [ ] The Red LED then turns on and off every second, forever.
+// [x] The Red LED then turns on and off every second, forever.
 // [ ] The Green LED simultaneously turns on and off every half-second, forever.
 // [ ] The Blue LED is on whenever the Green LED is off, and vice-versa.
 
